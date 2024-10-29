9586f635a362f000515b0262aeffe513f2680f79
MarceloMosl <marcelomosl@icloud.com>
2024-10-29 13:01:16 -0700

    feat: fixing the switcher file

--- src/functions/switcher.py
+++ src/functions/switcher.py
@@ -6,14 +6,21 @@ from functions.spending_analysis.switcher import spending_analysis_swticher
 
 def switch(value):
     switcher = {
-        "1": lambda: csv_switch(value),  # CSV file related functions
-        "2": lambda: data_management_switch(value),  # Data-Management related functions
-        "3": lambda: data_vizualization_switch(
+        "0": lambda: csv_switch(value),  # Import a CSV file
+        "1": lambda: data_management_switch(value),  # View All Transactions
+        "2": lambda: data_management_switch(value),  # View Transactions by Date Range
+        "3": lambda: data_management_switch(value),  # Add a Transaction
+        "4": lambda: data_management_switch(value),  # Edit a Transaction
+        "5": lambda: data_management_switch(value),  # Delete a Transaction
+        "6": lambda: spending_analysis_swticher(value),  # Analyze Spending by Category
+        "7": lambda: spending_analysis_swticher(
             value
-        ),  # Data-Vizualization related functions
-        "4": lambda: spending_analysis_swticher(
+        ),  # Calculate Average Monthly Spending
+        "8": lambda: spending_analysis_swticher(value),  # Show Top Spending Category
+        "9": lambda: data_vizualization_switch(
             value
-        ),  # Spending Analysis related functions
+        ),  # Visualize Monthly Spending Trend
+        "10": lambda: csv_switch(value),  # Save Transactions to CSV
     }
     switcher.get(
         value, lambda: print("Invalid option, please choose a valid option.")
