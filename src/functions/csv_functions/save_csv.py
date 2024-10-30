fb3823ac9bcc69aa55007ba4fd4159b799a151cd
MarceloMosl <marcelomosl@icloud.com>
2024-10-30 14:17:28 -0700

    feat: save csv file

---
+++ src/functions/csv_functions/save_csv.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from tkinter import Tk, simpledialog
+from tkinter.filedialog import askdirectory
+
+
+def select_folder_and_filename():
+    Tk().withdraw()
+
+    folder_path = askdirectory(title="Select a folder")
+
+    if folder_path:
+        filename = simpledialog.askstring(
+            "Input", "Enter the filename (without extension .csv):"
+        )
+
+        if filename:
+            file_path = f"{folder_path}/{filename}.csv"
+            return file_path
+
+    return None
+
+
+def save_csv_file(df: pd.DataFrame):
+    filename = select_folder_and_filename()
+    if filename == None:
+        return
+
+    df.to_csv(filename)
+    print("File saved succesfully!")
